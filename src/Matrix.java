805e4c83a55fd436b3b78d2bd63403db711aac24
Mitesh <mitesh@thoughtworks.com>
2016-02-24 10:38:48 +0530

    multiplication function is added to the matrix

--- src/Matrix.java
+++ src/Matrix.java
@@ -1,52 +1,122 @@
-import java.util.Arrays;
+public class Matrix {
+	private int row;
+	private int column;
+	private int[][] matrix;
 
-class Matrix {
-     private int rows;
+	private Matrix(int row, int column){
+		this.row = row;
+		this.column = column;
+		this.matrix = new int[row][column];
+	}
 
-     private int columns;
+	private void initialise(int[] values){
+		for(int i = 0, count=0; i<row; i++){
+			for (int j=0; j<column ; j++,count++)
+				this.matrix[i][j] = values[count];
+		}
+	}
 
-     private int [][] matrix;
+	public int getValueAt(int row, int column){
+		return this.matrix[row][column];
+	}
 
-    public Matrix(int rows ,int columns){
-       this.rows = rows;
-       this.columns = columns;
-       this.matrix = new int[rows][columns];
-     }
-   public boolean isEqual(int[][] otherMatrix){
-        return  Arrays.deepEquals(this.matrix, otherMatrix);
-    }
-       Matrix insertIn(int row, int column,int values){
-       matrix[row][column] = values;
-       return this;
-     }
+  public static Matrix generateMatrix(int row, int column, int[] values){
+    Matrix matrix = new Matrix(row, column);
+    matrix.initialise(values);
+    return matrix;
+  }
 
-     private int valueAt(int row, int column) {
-           return matrix[row][column];
-    }
+	public Matrix addMatrices(Matrix other){
+		int[] add = new int[this.row*this.column];
+    int count = 0;
+		for(int i = 0; i<this.row; i++){
+			for (int j=0; j<this.column ; j++){
+				add[count] = this.getValueAt(i,j)+other.getValueAt(i,j);
+        count++;
+      }
+		}
+		return generateMatrix(this.row, this.column, add);
+	}
 
-    public Matrix hold(int value){
-        for(int row = 0; row < rows; row++)
-            Arrays.fill(matrix[row], value);
-         return this;
-   }
+	@Override
+	public boolean equals(Object other){
+		if (this == other)
+			return true;
+		if(!(other instanceof Matrix))
+			return false;
+		return this.equals((Matrix) other);
+	}
 
-   public Matrix setValues(int[][] matrix){
-           for(int row = 0; row < rows; row++)
-               for(int column = 0; column < columns; column++)
-                   this.matrix[row][column] = matrix[row][column];
-           return this;
-       }
+	public boolean equals(Matrix other){
+    int count=0;
+		for(int i = 0; i<this.row; i++){
+			for (int j=0; j<this.column ; j++){
+				if(this.getValueAt(i,j)!=other.getValueAt(i,j))
+					return false;
+        count++;
+			}
+		}
+		return true;
+	}
 
-    public Matrix add(Matrix addendMatrix){
-        Matrix resultMatrix = new Matrix(rows, columns);
-        for (int row = 0; row < rows ; row++){
-            for (int column = 0; column < columns ; column++){
-                int addedValue = this.valueAt(row,column) + addendMatrix.valueAt(row,column);
-                resultMatrix.insertIn(row, column, addedValue);
-            }
-        }
-        return resultMatrix;
-    }
+	public Matrix multiplyMatrices(Matrix other){
+		if(this.column != other.row)
+			return null;
+		int[] result = new int[this.row*other.column];
+		int count= 0;
+		for (int i=0; i<this.row ; i++ ) {
+			for (int j=0 ; j<other.column ;j++) {
+				for (int k=0; k<this.column ; k++ )
+					result[count] += this.matrix[i][k] * other.matrix[k][j];
+				count++;
+			}
+		}
+		return generateMatrix(this.row, other.column, result);
+	}
 
+	private int[] multiplyWithRow(int[] row, int[][] matrix){
+		int[] result = new int[matrix.length];
+		for(int i = 0; i<matrix[0].length; i++){
+			int value = 0;
+			for(int j=0; j<row.length; j++)
+				value += row[j]*matrix[j][i];
+			result[i] = value;
+		}
+		return result;
+	}
 
+	private boolean isSqaureMatrix(){
+		return this.column == this.row;
+	}
+
+	public int determinant(){
+		if(!this.isSqaureMatrix())
+			return 0;
+		if(matrix.length == 1){
+			return matrix[0][0];
+		}
+		int value = 0;
+		int sign = 1;
+		for (int i=0; i<matrix[0].length; i++){
+			Matrix subMatrix = generateMatrix(this.row-1, this.column-1, deleteElement(matrix, 0, i));
+			value += matrix[0][i] * subMatrix.determinant() * sign;
+			sign *= -1;
+		}
+		return value;
+	}
+
+	private int[] deleteElement(int[][] matrix, int row, int column){
+		int[] resultantMatrix =  new int[(matrix.length-1) * (matrix[0].length-1)];
+		for (int i=0,count=0 ;i<matrix.length; i++ ){
+			if(i!=row){
+				for (int j=0; j<matrix[0].length; j++){
+					if(j!=column){
+						resultantMatrix[count] = matrix[i][j];
+						count++;
+					}
+				}
+			}
+		}
+		return resultantMatrix;
+	}
 }
--- test/MatrixTest.java
+++ test/MatrixTest.java
@@ -1,98 +1,104 @@
-import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.*;
 import org.junit.Test;
-import static org.junit.Assert.assertArrayEquals;
-import static org.junit.Assert.assertTrue;
-import static org.junit.Assert.assertFalse;
 
 public class MatrixTest {
+
+	@Test
+	public void generateMatrix_generate_matrix_with_given_size_and_insert_given_values(){
+		int[] a = {1,2,3,4,5,6};
+    Matrix matrix1 = Matrix.generateMatrix(2,3,a);
+
+		for(int i = 0, count=0; i<2; i++){
+			for (int j=0; j<3 ; j++,count++){
+				assertEquals(matrix1.getValueAt(i,j),a[count]);
+			}
+		}
+	}
+
     @Test
-    public void fill_fills_the_matrix_with_the_given_value() {
-        Matrix matrix = new Matrix(3,3);
-        int[][] expected = new int[][]{
-            {0,0,0},
-            {0,0,0},
-            {0,0,0}
-        };
-        assertTrue (matrix.hold(0).isEqual(expected));
-    }
-    @Test
-    public void fill_fills_the_matrix_with_the_given_value_that_is_not_given() {
-        Matrix matrix = new Matrix(3,3);
-        int[][] expected = new int[][]{
-            {1,2,3},
-            {3,0,5},
-            {2,4,8}
-        };
-        assertFalse (matrix.hold(0).isEqual(expected));
-    }
-    @Test
-    public void setValues_sets_the_matrix_with_given_matrix_values() {
-        Matrix matrix = new Matrix(3,4);
-        int[][] actual = new int[][]{
-            {0,3,0,8},
-            {0,0,5,1},
-            {2,0,0,6}
-        };
-        assertTrue(matrix.setValues(actual).isEqual(actual));
+    public void add_adds_the_given_matrix_with_the_matrix(){
+    	int[] a = {1,2,3,4,5,6};
+    	int[] b = {6,5,4,3,2,1};
+        Matrix matrix1 = Matrix.generateMatrix(2,3,a);
+        Matrix matrix2 = Matrix.generateMatrix(2,3,b);
+
+        int[] expectedArray = {7,7,7,7,7,7,7};
+        Matrix expected = Matrix.generateMatrix(2,3,expectedArray);
+
+        Matrix result = matrix1.addMatrices(matrix2);
+
+        assertEquals(expected,result);
     }
 
-   @Test
-   public void add_adds_two_matricies_and_return_a_new_matrix(){
-      Matrix first_matrix  = new Matrix(3,3);
-      Matrix second_matrix = new Matrix(3,3);
+    @Test
+    public void multiply_returns_null_if_the_two_matrices_cant_be_multiplied(){
+    	int[] a = {1,2,3,4,5,6};
+    	int[] b = {6,5,4,3,2,1};
+        Matrix matrix1 = Matrix.generateMatrix(2,3,a);
+        Matrix matrix2 = Matrix.generateMatrix(2,3,b);
 
-      first_matrix.hold(2);
-      second_matrix.hold(3);
+    	assertNull(matrix1.multiplyMatrices(matrix2));
+    }
 
-      int[][]first_matrix_value = new int[][]{
-            {2,2,2},
-            {2,2,2},
-            {2,2,2}
-        };
-        int[][] second_matrix_value = new int[][]{
-            {3,3,3},
-            {3,3,3},
-            {3,3,3}
-        };
+    @Test
+    public void multiply_returns_the_product_of_two_matrix_of_same_sizes(){
+    	int[] a = {1,2};
+    	int[] b = {1,2};
+        Matrix matrix1 = Matrix.generateMatrix(1,2,a);
+        Matrix matrix2 = Matrix.generateMatrix(2,1,b);
 
-        Matrix sum1 = first_matrix.add(second_matrix);
-        int[][] expected1 = new int[][]{
-            {5,5,5},
-            {5,5,5},
-            {5,5,5}
-        };
+        int[] expectedArray = {5};
+        Matrix expected = Matrix.generateMatrix(1,1,expectedArray);
 
-        assertTrue(sum1.isEqual(expected1));
-        assertTrue(first_matrix.isEqual(first_matrix_value));
-        assertTrue(second_matrix.isEqual(second_matrix_value));
-   }
-   @Test
-   public void add_adds_two_matricies_with_diffrent_values_and_return_a_new_matrix(){
-      Matrix third_matrix  = new Matrix(3,3);
-      Matrix fourth_matrix = new Matrix(3,3);
+    	Matrix resultant = matrix1.multiplyMatrices(matrix2);
 
-      int[][]third_matrix_value = new int[][]{
-          {2,3,4},
-          {0,1,5},
-          {8,9,7}
-      };
-      int[][] fourth_matrix_value = new int[][]{
-          {4,5,6},
-          {9,8,8},
-          {3,2,1}
-      };
-      Matrix sum2 = third_matrix.add(fourth_matrix);
-      int[][] expected2 = new int[][]{
-          {6,8,10},
-          {9,9,13},
-          {11,11,8}
-      };
-      // // System.out.println(third_matrix.toString());
-      // // System.out.println(Matrix sum2);
-      //
-      // //   assertTrue("__________",sum2.isEqual(expected2));
-      //   assertTrue(third_matrix.isEqual(third_matrix_value));
-      // //   assertTrue(fourth_matrix.isEqual(fourth_matrix_value));
+    	assertEquals(expected,resultant);
+    }
 
-}
+    @Test
+    public void multiply_returns_the_product_of_two_matrix_of_different_sizes(){
+    	int[] a = {1,2,3,4,5,6};
+    	int[] b = {1,2,3,4,5,6};
+      Matrix matrix1 = Matrix.generateMatrix(2,3,a);
+      Matrix matrix2 = Matrix.generateMatrix(3,2,b);
+
+      int[] expectedArray = {22,28,49,64};
+      Matrix expected = Matrix.generateMatrix(2,2,expectedArray);
+
+    	Matrix resultant = matrix1.multiplyMatrices(matrix2);
+
+    	assertEquals(expected,resultant);
+    }
+
+    @Test
+    public void determinant_returns_zero_if_the_matrix_have_no_determinant_like_2x3(){
+    	int[] a = {1,2,3,4,5,6};
+      Matrix matrix1 = Matrix.generateMatrix(2,3,a);
+
+    	assertEquals(matrix1.determinant(),0);
+    }
+
+    @Test
+    public void determinant_returns_the_determinant_of_simple_2X2_matrix_with_value_negative_2(){
+    	int[] a = {1,2,3,4};
+      Matrix matrix1 = Matrix.generateMatrix(2,2,a);
+
+    	assertEquals(-2,matrix1.determinant());
+    }
+
+    @Test
+    public void determinant_returns_the_determinant_of_the_given_3x3_matrix_with_value_0(){
+    	int[] a = {1,2,3,4,5,6,7,8,9};
+      Matrix matrix1 = Matrix.generateMatrix(3,3,a);
+
+    	assertEquals(0,matrix1.determinant());
+    }
+
+    @Test
+    public void determinant_returns_the_determinant_of_the_given_3x3_matrix_with_value_3(){
+    	int[] a = {3,2,1,2,4,3,1,1,1};
+      Matrix matrix1 = Matrix.generateMatrix(3,3,a);
+
+    	assertEquals(3,matrix1.determinant());
+    }
 }
